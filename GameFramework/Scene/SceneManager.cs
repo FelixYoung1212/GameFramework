c85358b8cf3cbacf8f20c12aac2c560a081de532
FeiYang <672466295@qq.com>
2025-12-08 15:30:26 +0800

    因为Addressables中也包含场景加载，所有原来的SceneManager也不需要了

--- GameFramework/Scene/SceneManager.cs
+++ GameFramework/Scene/SceneManager.cs
@@ -1,4 +1,5 @@
-﻿//------------------------------------------------------------
+﻿#if !ADDRESSABLES_SUPPORT
+//------------------------------------------------------------
 // Game Framework
 // Copyright © 2013-2021 Jiang Yin. All rights reserved.
 // Homepage: https://gameframework.cn/
@@ -506,3 +507,4 @@ namespace GameFramework.Scene
         }
     }
 }
+#endif
