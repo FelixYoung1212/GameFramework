a7613adc7d471e417718e1319ae0bdc1a0b212af
FelixYoung <672466295@qq.com>
2025-12-08 19:51:27 +0800

    IAddressablesManager中新增带priority参数的加载资源的方法

--- GameFramework/Resource/Addressables/IAddressablesManager.cs
+++ GameFramework/Resource/Addressables/IAddressablesManager.cs
@@ -10,6 +10,15 @@ namespace GameFramework.Resource.Addressables
         /// 异步加载资源。
         /// </summary>
         /// <param name="assetName">要加载资源的名称。</param>
+        /// <param name="priority">加载资源的优先级。</param>
+        /// <param name="loadAssetCallbacks">加载资源回调函数集。</param>
+        /// <param name="userData">用户自定义数据。</param>
+        void LoadAsset(string assetName, int priority, LoadAssetCallbacks loadAssetCallbacks, object userData);
+        
+        /// <summary>
+        /// 异步加载资源。
+        /// </summary>
+        /// <param name="assetName">要加载资源的名称。</param>
         /// <param name="loadAssetCallbacks">加载资源回调函数集。</param>
         /// <param name="userData">用户自定义数据。</param>
         void LoadAsset(string assetName, LoadAssetCallbacks loadAssetCallbacks, object userData);
