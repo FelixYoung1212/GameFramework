690556b9ac35647296f6e55e4169cb3fddcb84ee
FeiYang <672466295@qq.com>
2025-12-08 17:15:08 +0800

    新增IAddressableManager

---
+++ GameFramework/Resource/Addressables/IAddressablesManager.cs
@@ -0,0 +1,16 @@
+﻿namespace GameFramework.Resource.Addressables
+{
+    /// <summary>
+    /// Addressables资源管理器接口
+    /// </summary>
+    public interface IAddressablesManager
+    {
+        /// <summary>
+        /// 异步加载资源。
+        /// </summary>
+        /// <param name="assetName">要加载资源的名称。</param>
+        /// <param name="loadAssetCallbacks">加载资源回调函数集。</param>
+        /// <param name="userData">用户自定义数据。</param>
+        void LoadAsset(string assetName, LoadAssetCallbacks loadAssetCallbacks, object userData);
+    }
+}
