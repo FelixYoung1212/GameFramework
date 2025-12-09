0d5d780c8e7c6f908243c395b35a06a051853f94
FeiYang <672466295@qq.com>
2025-12-09 18:28:10 +0800

    IAddressablesManager重构

--- GameFramework/Resource/Addressables/IAddressablesManager.cs
+++ GameFramework/Resource/Addressables/IAddressablesManager.cs
@@ -7,6 +7,22 @@ namespace GameFramework.Resource.Addressables
     public interface IAddressablesManager
     {
         /// <summary>
+        /// 获取当前资源适用的游戏版本号。
+        /// </summary>
+        string ApplicableGameVersion
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 获取当前内部资源版本号。
+        /// </summary>
+        int InternalResourceVersion
+        {
+            get;
+        }
+        
+        /// <summary>
         /// 异步加载资源。
         /// </summary>
         /// <param name="assetName">要加载资源的名称。</param>
@@ -46,6 +62,12 @@ namespace GameFramework.Resource.Addressables
         /// <param name="unloadSceneCallbacks">卸载场景回调函数集。</param>
         /// <param name="userData">用户自定义数据。</param>
         void UnloadScene(string sceneAssetName, UnloadSceneCallbacks unloadSceneCallbacks, object userData);
+        
+        /// <summary>
+        /// 卸载资源。
+        /// </summary>
+        /// <param name="asset">要卸载的资源。</param>
+        void UnloadAsset(object asset);
     }
 }
 #endif
